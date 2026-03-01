a8fad490be7697f9d465c3152d64633aef20f85c
cameron Casto <cameron.casto2@gmail.com>
2026-03-01 13:49:17 -0500

    Product-Catalog

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,9 +1,33 @@
-function Products() { 
-    return(
-        <div>
-            <h2>Products</h2>
-        </div>
-    );
+import data from "./data";
+
+function Products() {
+
+  return (
+    <div className="content">
+      <ul className="products">
+        {data.products.map((product, idx) => (
+          <li key={`${product.name}-${idx}`}>
+            <div className="product">
+              <img
+                className="product-image"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+              <div className="product-name">
+                <a href="/product">{product.name}</a>
+              </div>
+              <div className="product-brand">{product.brand}</div>
+              <div className="product-price">${product.price}</div>
+              <div className="product-rating">
+                {product.rating} Stars ({product.numberOfReviews} reviews)
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Products;
+
